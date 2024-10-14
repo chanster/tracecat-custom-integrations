d7f57a62039501375c5009bd6aa19b3d980ec2e5
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-10-14 01:37:50 -0700

    test add udf

--- udfs/greetings.py
+++ udfs/greetings.py
@@ -1,4 +1,4 @@
-from tracecat_registry import registry, RegistrySecret
+from tracecat_registry import registry, RegistrySecret, secrets
 
 test_secret = RegistrySecret(name="test_secret", keys=["KEY"])
 
@@ -28,4 +28,18 @@ def say_goodbye():
     print("Goodbye")
     return {
         "message": "Said goodbye successfully.  Update 16. Update 17. Update 18. Update 19."
+    }
+
+
+@registry.register(
+    default_title="Say Goodbye",
+    display_group="Greetings",
+    description="This is a function that says goodbye",
+    secrets=[test_secret],
+    namespace="integrations.greetings",
+)
+def different_goodbye():
+    return {
+        "message": "Said goodbye successfully.  Update 16. Update 17. Update 18. Update 19.",
+        "secret": secrets.get("test_secret"),
     }
