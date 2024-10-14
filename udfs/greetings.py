d83684627244176f9b0e1152b4f8ed3302bc400f
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-10-13 20:53:46 -0700

    Update goodbye 17

--- udfs/greetings.py
+++ udfs/greetings.py
@@ -26,6 +26,4 @@ def say_hello_world():
 )
 def say_goodbye():
     print("Goodbye")
-    return {
-        "message": "Said goodbye successfully. Update 13. Update 14. Update 15. Update 16!!!!"
-    }
+    return {"message": "Said goodbye successfully.  Update 16. Update 17"}
