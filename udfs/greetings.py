f945f3534c07192d7da8c4fb758b41a366fcc329
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-10-14 15:57:37 -0700

    Revert "Remove hello world"
    This reverts commit cf41cc95241d3810f82a3808c3ae14f04ffdceeb.

--- udfs/greetings.py
+++ udfs/greetings.py
@@ -4,6 +4,20 @@ test_secret = RegistrySecret(name="test_secret", keys=["KEY"])
 
 
 @registry.register(
+    default_title="Say Hello World",
+    display_group="Greetings",
+    description="This is a function that says hello world",
+    secrets=[test_secret],
+    namespace="integrations.greetings",
+)
+def say_hello_world():
+    print("Hello World")
+    return {
+        "message": "Said hello world successfully. This is a test that we can update the udf. And I've updated this again."
+    }
+
+
+@registry.register(
     default_title="Say Goodbye",
     display_group="Greetings",
     description="This is a function that says goodbye",
